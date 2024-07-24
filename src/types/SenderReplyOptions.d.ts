8e63c093ea46948e52f060872fe11967b86d4057
Jarco-dev <jarco@familiekers.nl>
2024-07-24 21:41:39 +0200

    fix: langType not being defined

--- src/types/SenderReplyOptions.d.ts
+++ src/types/SenderReplyOptions.d.ts
@@ -2,7 +2,7 @@ import { SenderMessageOptions, SenderReplyMethod } from "@/types";
 
 export interface SenderReplyOptions extends SenderMessageOptions {
     method?: SenderReplyMethod;
+    langType?: "EMBED" | "STRING";
     langLocation?: string;
-    langType?: LangContentType;
     langVariables?: { [key: string]: string };
 }
